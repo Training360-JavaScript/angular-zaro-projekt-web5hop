ad8f889fe208d6970c714416ae41324b78acc68e
Gabor Szasz <skywallalp@gmail.com>
2022-02-13 13:30:34 +0100

    added edit-bill to routes

--- web5hop/src/app/app-routing.module.ts
+++ web5hop/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BillComponent } from './page/bill/bill.component';
+import { EditBillComponent } from './page/edit-bill/edit-bill.component';
 
 const routes: Routes = [
   {
     path: 'bill',
     component: BillComponent,
+  },
+  {
+    path: 'edit-bill/:id',
+    component: EditBillComponent
   }
 ];
 
