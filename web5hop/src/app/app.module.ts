afd84a2b2b90362d4cb71345a51297eeaabc37b2
Gabor Szasz <skywallalp@gmail.com>
2022-02-13 08:57:31 +0100

    module fix

--- web5hop/src/app/app.module.ts
+++ web5hop/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +24,7 @@ import { FilterPipe } from './pipe/filter.pipe';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
 
   ],
   providers: [],
