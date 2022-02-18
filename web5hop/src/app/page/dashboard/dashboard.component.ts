29ddd3d89f6a6075e67c3fbe8c794d68a5e773b5
Gabor Szasz <skywallalp@gmail.com>
2022-02-19 00:04:51 +0100

    products data od dashboard

--- web5hop/src/app/page/dashboard/dashboard.component.ts
+++ web5hop/src/app/page/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { BillService } from './../../service/bill.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Bill } from 'src/app/model/bill';
+import { BillService } from './../../service/bill.service';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,15 +11,25 @@ import { Bill } from 'src/app/model/bill';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  unpaidBills: number = 0;
-  list$: Observable<Bill[]> = this.billService.getAll();
+  billsNew: number = 0;
+  billList$: Observable<Bill[]> = this.billService.getAll();
+  productsActive: number = 0;
+  productList$: Observable<Product[]> = this.productService.getAll()
 
-  constructor(private billService: BillService) {}
+  constructor(
+    private billService: BillService,
+    private productService: ProductService
+    ) {}
 
   ngOnInit(): void {
-    this.list$.subscribe({
+    this.billList$.subscribe({
       next: (data) => {
-        this.unpaidBills = data.filter((b) => b.status === 'new').length;
+        this.billsNew = data.filter((b) => b.status === 'new').length;
+      },
+    });
+    this.productList$.subscribe({
+      next: (data) => {
+        this.productsActive = data.filter((b) => b.active === true).length;
       },
     });
   }
